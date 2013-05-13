a95718daa6ae2a4e0966d51267c4b82d52b51708
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-05-13 19:21:36 +0200

    more

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 
 
-import dis, opcode
+import dis
+
+def _modify_abs_jumps(codestr, start, end, jumprel):
+	i = start
+	while i < end:
+		op = ord(codestr[i])
+		i += 1
+
+		if op >= dis.HAVE_ARGUMENT:
+			b1 = ord(codestr[i])
+			b2 = ord(codestr[i+1])
+			num = b2 * 256 + b1
+			del b1,b2
+			i += 2
+		else:
+			num = 0
+
+		if op in dis.hasjabs:
+			assert op >= dis.HAVE_ARGUMENT
+			num += jumprel
+			codestr[i-2] = chr(num & 255)
+			codestr[i-1] = chr(num >> 8)
+
+def _join_codestr(codestr1, codestr2):
+	codestr = codestr1 + codestr2
+	_modify_abs_jumps(codestr, start=len(codestr1), end=len(codestr), jumprel=len(codestr1))
+	return codestr
 
 def restart_func(func, codeline, localdict):
 
@@ -20,6 +46,6 @@ def restart_func(func, codeline, localdict):
 		else:
 			num = 0
 
-		if op in opcode.hasjabs:
-			print opcode.opname[op] + " to " + repr(num)
+		if op in dis.hasjabs:
+			print dis.opname[op] + " to " + repr(num)
 

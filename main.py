43a3037f6f0eaa00572e39abd04b86d341a5e325
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-05-14 01:41:46 +0200

    more

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import dis
 import types
 
 
-def _modify_abs_jumps(codestr, start, end, jumprel):
+def _modified_abs_jumps(codestr, start, end, jumprel):
+	codestr = bytearray(codestr)
 	i = start
 	while i < end:
-		op = ord(codestr[i])
+		op = codestr[i]
 		i += 1
 
 		if op >= dis.HAVE_ARGUMENT:
-			b1 = ord(codestr[i])
-			b2 = ord(codestr[i+1])
+			b1 = codestr[i]
+			b2 = codestr[i+1]
 			num = b2 * 256 + b1
 			del b1,b2
 			i += 2
@@ -25,65 +26,72 @@ def _modify_abs_jumps(codestr, start, end, jumprel):
 			codestr[i-2] = chr(num & 255)
 			codestr[i-1] = chr(num >> 8)
 
+	return str(codestr)
+
 def _codestr_without_final_return(codestr):
 	assert len(codestr) >= 4
 	assert codestr[-4] == dis.opmap["LOAD_CONST"]
 	assert codestr[-1] == dis.opmap["RETURN_VALUE"]
 	return codestr[:-4]
 
-def _join_codestr(codestr1, codestr2, firstlineno2, lnotab2):
+def _prefix_codestr(codestr1, codestr2):
 	# see dis.findlinestarts() about co_firstlineno and co_lnotab
-	codestr1 = _codestr_without_final_return(codestr1)
 	codestr = codestr1 + codestr2
-	_modify_abs_jumps(codestr, start=len(codestr1), end=len(codestr), jumprel=len(codestr1))
+	codestr = _modified_abs_jumps(codestr, start=len(codestr1), end=len(codestr), jumprel=len(codestr1))
 	return codestr
 
-def _modify_code(c, codestr):
-
+def _modified_code(c, **kwargs):
 	CodeArgs = [
 		"argcount", "nlocals", "stacksize", "flags", "code",
 		"consts", "names", "varnames", "filename", "name",
 		"firstlineno", "lnotab", "freevars", "cellvars"]
 	c_dict = dict([(arg, getattr(c, "co_" + arg)) for arg in CodeArgs])
-	c_dict["code"] = codestr
+
+	for key,value in kwargs.items():
+		assert key in c_dict
+		c_dict[key] = value
 
 	c = types.CodeType(*[c_dict[arg] for arg in CodeArgs])
 	return c
-
-def _merge_locals(localdict):
-	#locals().update(localdict)
-	a = 42
 
 def restart_func(func, instraddr, localdict):
 	preload_code = ""
 	code_consts = func.func_code.co_consts
 	LOAD_CONST = chr(dis.opmap["LOAD_CONST"])
+	STORE_FAST = chr(dis.opmap["STORE_FAST"])
 	for key,value in localdict.items():
 		co_const_idx = len(code_consts)
 		code_consts += (value,)
-		preload_code += LOAD_CONST
-		preload_code += chr(co_const_idx & 255) + chr(co_const_idx >> 8)
+		preload_code += LOAD_CONST + chr(co_const_idx & 255) + chr(co_const_idx >> 8)
+		varidx = func.func_code.co_varnames.index(key)
+		preload_code += STORE_FAST + chr(varidx & 255) + chr(varidx >> 8)
 	instraddr += len(preload_code) + 3 # 3 for the following jump_abs
 	preload_code += chr(dis.opmap["JUMP_ABSOLUTE"])
-	
-	end = len(func.func_code.co_code)
-	i = 0
-	while i < end:
-		op = ord(func.func_code.co_code[i])
-		i += 1
-		name = dis.opname[op]
+	preload_code += chr(instraddr & 255) + chr(instraddr >> 8)
 
-		if op >= dis.HAVE_ARGUMENT:
-			b1 = ord(func.func_code.co_code[i])
-			b2 = ord(func.func_code.co_code[i+1])
-			num = b2 * 256 + b1
-			del b1,b2
-			i += 2
-		else:
-			num = 0
+	codestr = _prefix_codestr(preload_code, func.func_code.co_code)
 
-		if op in dis.hasjabs:
-			print dis.opname[op] + " to " + repr(num)
+	lnotab = func.func_code.co_lnotab
+	lnotab_moverel = len(preload_code)
+	while lnotab_moverel > 0:
+		lnotab = chr(lnotab_moverel & 255) + chr(0) + lnotab
+		lnotab_moverel -= lnotab_moverel & 255
+
+	new_code = _modified_code(
+		func.func_code,
+		consts=code_consts,
+		code=codestr,
+		lnotab=lnotab,
+		argcount=0
+	)
+	new_func = types.FunctionType(
+		new_code,
+		func.func_globals,
+		func.func_name,
+		func.func_defaults,
+		func.func_closure,
+	)
+	return new_func
 
 def demoFunc(a,b,c, raiseExc=None):
 	print("a: %r" % a)
@@ -100,14 +108,26 @@ def _find_traceframe(tb, code):
 	return None
 
 def demo():
+	func = demoFunc
 	try:
 		demoFunc(5,4,1, Exception)
+		assert False
 	except Exception:
 		import sys
 		_,_,tb = sys.exc_info()
-		tb = _find_traceframe(tb, demoFunc.func_code)
-		localdict = tb.tb_frame.f_locals
-		lineno = tb.tb_lineno #, tb.tb_frame.f_lineno
-		instaddr = tb.tb_lasti # tb.tb_frame.f_lasti
 
+	tb = _find_traceframe(tb, func.func_code)
+	assert tb is not None
+	localdict = tb.tb_frame.f_locals
+	lineno = tb.tb_lineno #, tb.tb_frame.f_lineno
+	instraddr = tb.tb_lasti # tb.tb_frame.f_lasti
+
+	instraddr += 3 if ord(func.func_code.co_code[instraddr]) >= dis.HAVE_ARGUMENT else 1
 	localdict["b"] = 2
+	new_func = restart_func(func, instraddr=instraddr, localdict=localdict)
+
+	return new_func
+
+if __name__ == "__main__":
+	f = demo()
+	f()

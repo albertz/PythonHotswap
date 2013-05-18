c7f382eeafde519737f87a710420c583cad98750
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-05-18 18:15:08 +0200

    demo4 wip

--- main.py
+++ main.py
@@ -194,6 +194,38 @@ def demo3():
 	new_func()
 
 
+def demo4():
+	def func():
+		bug = True
+		for i in range(2):
+			print "a:", i
+		for i in range(2):
+			print "b:", i
+			if bug: raise Exception
+
+	dis.dis(func)
+	return func
+
+	try:
+		func()
+		assert False
+	except Exception:
+		print "! Exception"
+		_,_,tb = sys.exc_info()
+
+	tb = _find_traceframe(tb, func.func_code)
+	assert tb is not None
+
+	# Start just at where the exception was raised.
+	instraddr = max([addr for (addr,_) in dis.findlinestarts(func.func_code) if addr <= tb.tb_lasti])
+	# Play around. Avoid that we throw the exception again.
+	localdict = dict(tb.tb_frame.f_locals)
+	localdict["bug"] = False
+	new_func = restart_func(func, instraddr=instraddr, localdict=localdict)
+
+	new_func()
+
+
 if __name__ == "__main__":
 	demoname = "demo1"
 	if len(sys.argv) > 0: demoname = sys.argv[1]
